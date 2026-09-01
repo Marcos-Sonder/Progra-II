788c2e24b6aadf122bb8e0c291a328055a903e96
Marcos-Sonder <marcosapaza987@gmail.com>
2026-08-31 21:36:37 -0400

    Implement sorting of random numbers with timing
    This program generates an array of random numbers and sorts them using selection sort while measuring the time taken for the sorting process.

--- Practica01/Ejercicio01/TestCronometro.java
+++ Practica01/Ejercicio01/TestCronometro.java
@@ -1,1 +1,41 @@
+import java.util.Random;
 
+public class TestCronometro {
+
+    public static void main(String[] args) {
+
+        double[] numeros = new double[100000];
+
+        Random random = new Random();
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = random.nextDouble() * 100000;
+        }
+
+        Cronometro cronometro = new Cronometro();
+
+        cronometro.inicia();
+
+        for (int i = 0; i < numeros.length - 1; i++) {
+
+            int indiceMinimo = i;
+
+            for (int j = i + 1; j < numeros.length; j++) {
+
+                if (numeros[j] < numeros[indiceMinimo]) {
+                    indiceMinimo = j;
+                }
+            }
+
+            double temporal = numeros[i];
+            numeros[i] = numeros[indiceMinimo];
+            numeros[indiceMinimo] = temporal;
+        }
+
+        cronometro.detener();
+
+        System.out.println("Tiempo de ordenamiento: "
+                + cronometro.lapsoDeTiempo()
+                + " milisegundos");
+    }
+}
